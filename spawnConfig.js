19f3222a78d9603d9dff533ec36d55ccf463fff6
roblol <rabblerabble.wtf@gmail.com>
2018-07-26 21:03:09 +0200

    fix config

--- spawnConfig.js
+++ spawnConfig.js
@@ -1,74 +1,74 @@
 module.exports = {
     creeps: {
         'basic': {
-            0: {
+            '0': {
                 energy: 0,
                 config: [WORK, MOVE, CARRY]
             }
         },
         'builder': {
-            3: {
+            '3': {
                 energy: 1300,
                 config: [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
             },
-            2: {
+            '2': {
                 energy: 800,
                 config: [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
             },
-            1: {
+            '1': {
                 energy: 550,
                 config: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE]
             },
-            0: {
+            '0': {
                 energy: 0,
                 config: [WORK, MOVE, CARRY]
             }
         },
         'harvester': {
-            3: {
+            '3': {
                 energy: 1300,
                 config: [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
             },
-            2: {
+            '2': {
                 energy: 800,
                 config: [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE]
             },
-            1: {
+            '1': {
                 energy: 550,
                 config: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE]
             },
-            0: {
+            '0': {
                 energy: 0,
                 config: [WORK, MOVE, CARRY]
             }
         },
         'repairer': {
-            1: {
+            '1': {
                 energy: 550,
                 config: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE]
             },
-            0: {
+            '0': {
                 energy: 0,
                 config: [WORK, MOVE, CARRY]
             }
         },
         'upgrader': {
-            3: {
+            '3': {
                 energy: 1300,
                 config: [WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
             },
-            2: {
+            '2': {
                 energy: 800,
                 config: [WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE]
             },
-            1: {
+            '1': {
                 energy: 550,
                 config: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE]
-            }
-            0: {
+            },
+            '0': {
                 energy: 0,
                 config: [WORK, MOVE, CARRY]
-            },
+            }
         }
     }
 };
